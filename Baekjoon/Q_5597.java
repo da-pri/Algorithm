21ffdf55271594a823926371c5b3b907abab9e5d
dapri <102639884+da-pri@users.noreply.github.com>
2022-09-15 17:09:25 +0900

    Add files via upload

---
+++ Baekjoon/Q_5597.java
@@ -0,0 +1,25 @@
+package SOLVED;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q_5597 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] list = new int[31];
+		
+		for(int i = 0; i < 28; i++) {
+			int num = sc.nextInt();
+			
+			list[num] = 1;
+		}
+		for(int i = 1; i < 30; i++) {
+			if(list[i] == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
