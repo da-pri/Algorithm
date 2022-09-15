4bcb870799ceeddbad989c79390ee0ba0918b4f6
dapri <102639884+da-pri@users.noreply.github.com>
2022-09-15 17:27:38 +0900

    Add files via upload

---
+++ Baekjoon/Q_10807.java
@@ -0,0 +1,28 @@
+package SOLVED;
+
+import java.util.Scanner;
+
+public class Q_10807 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		int cnt = 0;
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int v = sc.nextInt();
+		
+		for(int i = 0; i < arr.length; i++) {
+			if(v == arr[i]) {
+			cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+
+}
