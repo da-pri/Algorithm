4a7d9e3d5541b1250e60ff1fdf3fb4c73727e0cb
dapri <102639884+da-pri@users.noreply.github.com>
2022-09-16 16:56:08 +0900

    Add files via upload

---
+++ Baekjoon/Q_1230.java
@@ -0,0 +1,24 @@
+package _2022_05_07;
+
+import java.util.*;
+
+public class Q_1230 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+		int car = 170;
+
+		if (car < a && car < b && car < c) {
+			System.out.println("PASS");
+		} else if (car >= a) {
+			System.out.println("CRASH " + a);
+		} else if (car >= b) {
+			System.out.println("CRASH " + b);
+		} else if (car >= c) {
+			System.out.println("CRASH " + c);
+		}
+	}
+
+}
