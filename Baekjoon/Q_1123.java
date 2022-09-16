3a5f331f9bc77ea0352e50355dc9c9e91aa400fd
dapri <102639884+da-pri@users.noreply.github.com>
2022-09-16 16:52:49 +0900

    Add files via upload

---
+++ Baekjoon/Q_1123.java
@@ -0,0 +1,15 @@
+package _2022_05_07;
+import java.util.*;
+
+public class Q_1123 {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+		int C = sc.nextInt();
+		double F = (double)C*9/5+32;
+		
+		String st = String.format("%.3f", F);
+		System.out.printf(st);
+	}
+
+}
