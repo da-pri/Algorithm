561fa4e8e375d141905341d5e942d32384cfa460
dapri <0p25dy@naver.com>
2022-09-27 18:44:56 +0900

    가희와 방어율 무시

--- Baekjoon/Q_25238.java
+++ Baekjoon/Q_25238.java
@@ -6,16 +6,16 @@ public class Q_25238 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int a = sc.nextInt(), b = sc.nextInt();
-		
+
 		double tmp = a - a * (1d * b / 100);
-		
-		if(tmp >= 100) {
+
+		if (tmp >= 100) {
 			System.out.println(0);
-		}
-		else System.out.println(1);
-		
+		} else
+			System.out.println(1);
+
 	}
 
 }
