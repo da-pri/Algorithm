785579982976a0a29e524564310e736baf4dc583
YEON <102639884+da-pri@users.noreply.github.com>
2022-09-27 18:35:34 +0900

    가희와 방어율 무시

---
+++ Baekjoon/Q_25238.java
@@ -0,0 +1,21 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Q_25238 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt(), b = sc.nextInt();
+		
+		double tmp = a - a * (1d * b / 100);
+		
+		if(tmp >= 100) {
+			System.out.println(0);
+		}
+		else System.out.println(1);
+		
+	}
+
+}
