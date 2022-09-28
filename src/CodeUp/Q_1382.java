668b9565ac75295ea034e5ffda3ce8c4615442f1
Dayeon Yu <0p25dy@naver.com>
2022-09-28 18:36:02 +0900

    Update Q_1382.java

--- src/CodeUp/Q_1382.java
+++ src/CodeUp/Q_1382.java
@@ -7,8 +7,8 @@ public class Q_1382 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		for(int i = 1; i <= 9; i++) { // 열
-			for(int j = 2; j <= 5; j++) { // 행
+		for(int i = 1; i <= 9; i++) { 
+			for(int j = 2; j <= 5; j++) { 
 				System.out.printf("%d x %d = %2d\t", j,i,j*i);
 			}
 			System.out.println();
