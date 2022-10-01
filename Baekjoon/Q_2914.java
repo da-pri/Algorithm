850a53b04ccb7c553fff782f667d32ce543fa4dc
dapri <0p25dy@naver.com>
2022-10-01 21:49:54 +0900

    저작권

---
+++ Baekjoon/Q_2914.java
@@ -0,0 +1,18 @@
+package Baekjoon;
+
+import java.util.*;
+
+public class Q_2914 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt(); // °îÀÇ °³¼ö
+		int b = sc.nextInt(); // Æò±Õ
+
+		int c = ((b - 1) * a + 1);
+
+		System.out.println(c);
+
+	}
+}
